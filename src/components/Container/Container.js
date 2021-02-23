93a2d2e79962b0ff80d52929f1fac305b00afa89
lauravikanis <lauravikanis@yahoo.de>
2021-02-23 21:04:20 +0100

    add container with tailwindCSS

---
+++ src/components/Container/Container.js
@@ -0,0 +1,14 @@
+const Container = () => {
+  return (
+    <div class="p-6 max-w-sm mx-auto bg-red-200	rounded-xl shadow-md flex items-center space-x-4">
+      <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div class="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-indigo-300 hover:bg-green-700">
+          <h1 class="font-serif ...">Hello</h1>
+          <h2 class="font-sans ...">Tailwind!</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Container;
